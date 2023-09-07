04c136e75c4d7f1fa38157aae4b43d1b788426ca
Prince <ranap8348@gmail.com>
2023-09-07 18:39:15 +0530

    configured git persist

--- src/index.js
+++ src/index.js
@@ -1,16 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { Provider } from 'react-redux';
-import { store } from './redux/Store';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import { store,persistor } from "./redux/Store";
 import "react-toastify/dist/ReactToastify.css";
-import './index.css';
-import App from './App';
+import { PersistGate } from "redux-persist/integration/react";
+import "./index.css";
+import App from "./App";
 
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-    <Provider store={store}>
-    <App />
-    </Provider>
+  <Provider store={store}>
+    <PersistGate Loading={"loading"} persistor={persistor}>
+      <App />
+    </PersistGate>
+  </Provider>
 );
-
--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import {useSelector} from 'react-redux';
+import { Link } from "react-router-dom";
+import { HiOutlineArrowLeft } from "react-icons/hi";
 import CartItem from '../components/CartItem';
 
 const Cart = () => {
@@ -21,6 +23,7 @@ const Cart = () => {
         src="https://images.pexels.com/photos/1435752/pexels-photo-1435752.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
         alt="cartImg"
       />
+      {productData.length>0 ?(
       <div className='max-w-screen-xl mx-auto py-20 flex'>
         <CartItem/>
         <div className='w-1/3 bg-[#fafafa] py-6 px-4'>
@@ -48,8 +51,25 @@ const Cart = () => {
             </button>
         </div>
       </div>
+      ):(
+        <div className="max-w-screen-xl mx-auto py-10 flex flex-col items-center gap-2 justify-center">
+        <p className="text-xl text-orange-600 font-titleFont font-semibold">
+          Your Cart is Empty. Please go back to Shopping and add products to
+          Cart.
+        </p>
+        <Link to="/">
+          <button className="flex items-center gap-1 text-gray-400 hover:text-black duration-300">
+            <span>
+              <HiOutlineArrowLeft />
+            </span>
+            go shopping
+          </button>
+        </Link>
+      </div>
+      )}
     </div>
   )
-}
+      }
+
 
 export default Cart
